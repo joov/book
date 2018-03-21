e65dc978bcd493c8bd1b25f7ce2937f7b4389735
jwenzel <johannes.wenzel@schoenhofer.de>
2018-03-21 15:23:41 +0100

    isbn validator ready

--- src/app/book/book-new/book-new.component.ts
+++ src/app/book/book-new/book-new.component.ts
@@ -22,15 +22,36 @@ export class BookNewComponent implements OnInit {
     private location: Location) {}
 
     validateIsbn(c: FormControl) {
-      console.log('validate called');
-      const ISBN_REGEX = new RegExp('(978|979)-(\d)+-(\d)+-(\d)+-(\d)' );
+      console.log('validation called');
+      const ISBN_REGEX = new RegExp('(978|979)-(\\d+)-(\\d+)-(\\d+)-(\\d)' );
 
-      return ISBN_REGEX.test(c.value) ? null : {
-          validateEmail: {
-              valid: false
-          }
-      };
+      const match: RegExpExecArray = ISBN_REGEX.exec(c.value);
+      console.log('match ' + match);
+      if (!match || match.length !== 6) {
+          return   {
+              validateEmail: {
+                  valid: false
+              }
+          };
+      }
+      const num = match[1] + match[2] + match[3] + match[4];
+      console.log(num);
+      let sum = 0;
+      for (let i = 0 ; i < num.length ; ++i) {
+          sum += parseInt(num.charAt(i), 10) * Math.pow(3, i % 2);
+      }
+      const result = (10 - sum % 10) % 10;
+      console.log(result);
+      if (result !== parseInt(match[5], 10)) {
+          return {
+              validateEmail: {
+                  valid: false
+              }
+          };
+      }
+      return null;
   }
+
 
   ngOnInit() {
     this.book = {title: '', isbn: '', subtitle: '', author: '', publisher: {name: '', url: ''}, abstract: ''} ;
--- src/app/book/share/IsbnValidatorDirective.ts
+++ src/app/book/share/IsbnValidatorDirective.ts
@@ -11,12 +11,34 @@ import { FormControl, NG_VALIDATORS } from '@angular/forms';
 
 export class IsbnValidatorDirective {
     validateIsbn(c: FormControl) {
-        const ISBN_REGEX = new RegExp('(978|979)-(\d)+-(\d)+-(\d)+-(\d)' );
+        console.log('validation called');
+        const ISBN_REGEX = new RegExp('(978|979)-(\\d)+-(\\d)+-(\\d)+-(\\d)' );
+        console.log('validation called2');
 
-        return ISBN_REGEX.test(c.value) ? null : {
-            validateEmail: {
-                valid: false
-            }
-        };
+        const match: RegExpExecArray = ISBN_REGEX.exec(c.value);
+        console.log('match ' + match);
+        if (match.length !== 5) {
+            return   {
+                validateEmail: {
+                    valid: false
+                }
+            };
+        }
+        const num = match[0] + match[1] + match[2] + match[3];
+        console.log(num);
+        let sum = 0;
+        for (let i = 0 ; i < num.length ; ++i) {
+            sum += parseInt(num.charAt(i), 10) * Math.pow(3, i % 2);
+        }
+        const result = (10 - sum % 10) % 10;
+        console.log(result);
+        if (result !== parseInt(match[4], 10)) {
+            return {
+                validateEmail: {
+                    valid: false
+                }
+            };
+        }
+        return null;
     }
 }
